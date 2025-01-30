226748618467572617adf94a50ca9b17023f325d
Enoch Nuertey <enochnuer111@gmail.com>
2025-01-30 13:46:44 +0000

    modified url

--- intro/urls.py
+++ intro/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('intro/', include("introapp.urls")),
+    path('', include("introapp.urls")),
 ]
 
 urlpatterns += static(
