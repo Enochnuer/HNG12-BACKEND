afaf53845935795b7e93b635df894452ec0fd53e
Enoch Nuertey <enochnuer111@gmail.com>
2025-01-30 13:37:46 +0000

    fixed base_dir

--- intro/settings.py
+++ intro/settings.py
@@ -13,10 +13,10 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 import django_heroku
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+django_heroku.settings(locals())
 
 load_dotenv()
 # Quick-start development settings - unsuitable for production
